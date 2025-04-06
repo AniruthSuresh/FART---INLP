59e554af7192ecb3bafa64930eb4ce52d9bbcb0e
AniruthSuresh <aniruthzlatan@gmail.com>
2025-04-06 19:53:01 +0530

    chore : check once

--- LRA/code/run_tasks.sh
+++ LRA/code/run_tasks.sh
@@ -3,3 +3,5 @@ python run_tasks.py --model dct-0.2 --task text --log_dir ../tb_logs/ --layers 0
 python run_tasks.py --model dct-0.2 --task retrieval --log_dir ../tb_logs/ --layers 0
 python run_tasks.py --model dct-0.2 --task image --log_dir ../tb_logs/ --layers 0
 python run_tasks.py --model dct-0.2 --task pathfinder32-curv_contour_length_14 --log_dir ../tb_logs/ --layers 0
+
+
--- LRA/datasets/cifar10.py
+++ LRA/datasets/cifar10.py
@@ -7,7 +7,7 @@ import numpy as np
 import pickle
 
 train_ds, eval_ds, test_ds, num_classes, vocab_size, input_shape = input_pipeline.get_cifar10_datasets(
-    n_devices = 1, batch_size = 1, normalize = False)
+    n_devices = 1, batch_size = 8, normalize = False)
 
 mapping = {"train":train_ds, "dev": eval_ds, "test":test_ds}
 for component in mapping:
